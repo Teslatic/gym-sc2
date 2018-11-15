1f0092cb6db2105091f93db0cf1d9997e1bc1326
Hendrik Vloet <vloeth@informatik.uni-freiburg.de>
2018-11-15 15:47:53 +0100

    added action fn, reward fn and parameterfile integration

--- gym_ghost/envs/gym_ghost.py
+++ gym_ghost/envs/gym_ghost.py
@@ -11,64 +11,58 @@ from pysc2.lib import actions, features
 import numpy as np
 import math
 
+from env_specs import mv2beacon_specs
+
 class GymSc2Env(gym.Env):
     """
     A wrapper class that uses the PYSC2 environment like a gym environment.
     """
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
-        self.visualize = True
+    def __init__(self, env_file = None):
         self.map_name = 'MoveToBeacon'
         self.players = [sc2_env.Agent(sc2_env.Race.terran)]
-        self.screen_dim = 84
+        self.screen_dim_x = 84
+        self.screen_dim_y = 64
         self.minimap_dim = 64
-        self.step_mul = 4
         self.game_steps = 0
-        self.save_replay = False,
-        self.replay_dir = None
         # observation space und action space anlegen
         self.agent_interface = self.setup_interface()
-        self.env = self.setup()
-        # first reset to get distance initialization of marine - beacon
         self.LAST_STEP = 2
+        self.MAX_DISTANCE = np.sqrt(self.screen_dim_x**2 + self.screen_dim_y**2)
+        self.MIN_DISTANCE = 0
 
-        self.factor = 10
-
-        self.reward_type = 'shaped'
-
-        self.action_type = 'grid'
-
-        if self.action_type == 'compass':
-            self.action_fn = self.compass_action_fn
-        elif self.action_type == 'grid':
+    def define_action_fn(self, action_type):
+        if action_type == 'compass':
+            return self.compass_action_fn
+        elif action_type == 'grid':
             self.xy_space = self.discretize_xy_grid()
-            self.action_fn = self.grid_action_fn
-        elif self.action_type == 'original':
-            self.action_fn = self.original_action_fn
+            return self.grid_action_fn
+        elif action_type == 'original':
+            return self.original_action_fn
         else:
             raise("Specifiy action function!")
 
-        if self.reward_type == 'shaped':
-            self.reward_fn = self.shaped_reward_fn
-        elif self.reward_type == 'sparse':
-            self.reward_fn = self.sparse_reward_fn
+    def define_reward_fn(self, reward_type):
+        if reward_type == 'diff':
+            return self.diff_reward_fn
+        elif reward_type == 'sparse':
+            return self.sparse_reward_fn
+        elif reward_type == 'distance':
+            return self.distance_reward_fn
         else:
             raise("Specify reward function!")
-        self.reset()
 
     def discretize_xy_grid(self):
         """ "discretizing" action coordinates in order to keep action space small """
-        x_space = np.linspace(0, 83, self.factor, dtype = int)
-        y_space = np.linspace(0, 63, self.factor, dtype = int)
+        x_space = np.linspace(0, 83, self.grid_factor, dtype = int)
+        y_space = np.linspace(0, 63, self.grid_factor, dtype = int)
         xy_space = np.transpose([np.tile(x_space, len(y_space)),
                                    np.repeat(y_space, len(x_space))])
 
         return xy_space
 
     def grid_action_fn(self, action):
-        print(self.xy_space, self.xy_space.shape)
-        print()
         if self.can_do(actions.FUNCTIONS.Move_screen.id):
             action = actions.FUNCTIONS.Move_screen("now",
                                         (self.xy_space[action][0], self.xy_space[action][1]))
@@ -127,45 +121,50 @@ class GymSc2Env(gym.Env):
 
 
     def sparse_reward_fn(self, observation):
-
         return observation[0].reward
 
-    def shaped_reward_fn(self, observation):
-
-        beacon_next, marine_next, distance_next = self.calc_distance(observation)
-
-
-        reward_shaped = self.distance - distance_next
+    def diff_reward_fn(self, observation):
+        reward_shaped = self.distance - self.distance_next
         if self.distance == 0.0:
             reward_shaped = 100
-
-        self.distance = distance_next
-        self.marine_center = marine_next
-        self.beacon_center = beacon_next
         return reward_shaped
 
+    def distance_reward_fn(self, observation):
+        scaling = lambda x : (x - self.MIN_DISTANCE)/(self.MAX_DISTANCE  - self.MIN_DISTANCE)
+        distance_reward = -1 * scaling(self.distance).round(4)
+
+        if observation[0].reward == 1:
+            return 10
+        else:
+            return distance_reward
 
     def setup_interface(self):
         """
         Setting up agent interface for the environment.
         """
+        # TODO(vloeth): only screen dimension x is used because of problems in case of using a tuple like (84, 64)
         agent_interface = features.AgentInterfaceFormat(
-                            feature_dimensions=features.Dimensions(screen=self.screen_dim, minimap=self.minimap_dim),
+                            feature_dimensions=features.Dimensions(screen=self.screen_dim_x, minimap=self.minimap_dim),
                             use_feature_units=True)
         return agent_interface
 
-    def setup(self):
-        env = sc2_env.SC2Env(
+    def setup(self, sc2_env_file):
+        self.env = sc2_env.SC2Env(
             map_name=self.map_name,
             players=self.players,
             agent_interface_format=self.agent_interface,
-            step_mul=self.step_mul,
+            step_mul = sc2_env_file['STEP_MUL'],
             game_steps_per_episode=self.game_steps,
-            visualize=self.visualize)
-            # save_replay_episodes=self.save_replay
-            # replay_dir=self.replay_dir)
+            visualize = sc2_env_file['VISUALIZE'],
+            # save_replay = sc2_env_file['SAVE_REPLAY'],
+            # replay_dir = sc2_env_file['REPLAY_DIR'],
+            )
 
-        return env
+        self.step_mul = sc2_env_file['STEP_MUL']
+        self.grid_factor = sc2_env_file['GRID_FACTOR']
+        self.action_fn = self.define_action_fn(sc2_env_file['ACTION_TYPE'])
+        self.reward_fn = self.define_reward_fn(sc2_env_file['REWARD_TYPE'])
+        self.reset()
 
     def retrieve_step_info(self, observation):
         """
@@ -173,18 +172,19 @@ class GymSc2Env(gym.Env):
         and converts it into gym-like observation tuple.
         """
         state = observation[0].observation.feature_screen.player_relative
+        beacon_next, marine_next, self.distance_next = self.calc_distance(observation)
         reward = self.reward_fn(observation)
         done = True if observation[0].step_type.value == self.LAST_STEP else False
         # check if needed
         info = None
 
         self.available_actions = observation[0].observation.available_actions
-        print(self.available_actions)
+
+        self.distance = self.distance_next
+        self.marine_center = marine_next
+        self.beacon_center = beacon_next
 
         return state, reward, done, info
-
-
-
 
     def step(self, action):
         pysc2_action = self.action_fn(action)
@@ -207,3 +207,7 @@ class GymSc2Env(gym.Env):
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
+
+
+
+
