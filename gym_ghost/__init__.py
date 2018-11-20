d94daf09be2fca58357523510152ba94fbba622b
Hendrik Vloet <vloeth@informatik.uni-freiburg.de>
2018-11-20 15:51:54 +0100

    adjusted to pep8

--- gym_ghost/__init__.py
+++ gym_ghost/__init__.py
@@ -1,8 +1,5 @@
 from gym.envs.registration import register
 
-
-
 register(
     id='sc2-v0',
-    entry_point='gym_ghost.envs:GymSc2Env'
-)
+    entry_point='gym_ghost.envs:GymSc2Env')
--- setup.py
+++ setup.py
@@ -2,7 +2,4 @@ from setuptools import setup
 
 setup(name='sc2-v0',
       version='0.0.1',
-      install_requires=['gym']
-)
-
-
+      install_requires=['gym'])
